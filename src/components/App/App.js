fc3ad386baeff04a03d6e123098883e67757488a
Rafael Castellanos-Welsh <mrrafael1912@gmail.com>
2020-06-02 22:56:42 -0700

    added props to reduce code

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import logo from '../../logo.svg';
 import './App.css';
+
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar'
+
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 PAddington Way',
+  city: 'Flavatown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+const businesses = [business, business, business, business, business, business];
 
 function App() {
   return (
     <div className="App">
       <h1>ravenous</h1>
       <SearchBar />
-      <BusinessList />
+      <BusinessList businesses={businesses} />
     </div>
   );
 }
